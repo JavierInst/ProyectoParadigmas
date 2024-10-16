7f0675f6af70c299118d2c130694e697e5e04e24
YorInst <145401538+YorInst@users.noreply.github.com>
2024-10-16 17:45:30 -0600

    Update factorialRecursivo.test.js

--- biesVM/tests/factorialRecursivo.test.js
+++ biesVM/tests/factorialRecursivo.test.js
@@ -1,16 +1,28 @@
 import antlr4 from 'antlr4';
 import BiesVMLexer from '../src/Grammar/BiesVMLexer.js';  // Asegúrate de que la ruta sea correcta
 
+/**
+ * Cadena de entrada que contiene instrucciones en lenguaje BiesVM.
+ * @type {string}
+ */
 const input = ` LDV 5     ; 
 LDV 120   ; 
 PRN;
 HLT;
 `;
 
+/**
+ * Crea un flujo de entrada a partir de la cadena de entrada y genera tokens utilizando el lexer BiesVM.
+ * @type {antlr4.InputStream}
+ */
 const chars = new antlr4.InputStream(input);
 const lexer = new BiesVMLexer(chars);
 const tokens = [];
 
+/**
+ * Genera tokens del lexer hasta que se alcance el final del archivo (EOF).
+ * @type {antlr4.Token}
+ */
 let token;
 do {
     token = lexer.nextToken();
@@ -18,7 +30,12 @@ do {
 } while (token.type !== antlr4.Token.EOF);
 // console.log("Tokens:", tokens.map(t => { return { type: t.type, text: t.text, line: t.line, column: t.column };}));
 
-
+/**
+ * Convierte un array de tokens en instrucciones para la máquina virtual BiesVM.
+ * 
+ * @param {antlr4.Token[]} tokens - Array de tokens generados por el lexer.
+ * @returns {string[]} Array de instrucciones generadas a partir de los tokens.
+ */
 function convertTokensToInstructions(tokens) {
     const instructions = [];
     let currentVariable = null;
@@ -65,5 +82,10 @@ function convertTokensToInstructions(tokens) {
     return instructions;
 }
 
+/**
+ * Convierte los tokens generados en instrucciones y las imprime en la consola.
+ * 
+ * @type {string[]} Array de instrucciones generadas a partir de los tokens.
+ */
 const instructions = convertTokensToInstructions(tokens);
 console.log(instructions);
